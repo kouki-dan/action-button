130c9434a2a67557383b959b5a4f25ccb62376aa
Kouki Saito <dan.addr.skd@gmail.com>
2019-09-24 23:10:36 +0900

    Write home details

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Typography, Button, Box } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
-
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles({
   detail: {
@@ -9,6 +9,9 @@ const useStyles = makeStyles({
   },
   explanation: {
     padding: "1rem 0"
+  },
+  body: {
+    padding: "0.5em 0"
   }
 })
 
@@ -24,36 +27,82 @@ const Home = () => {
           fontWeight: "bold",
           lineHeight: "1.6em"
       }} >This is a Button for<br/>GitHub Actions</Typography>
-      {/* TODO: Use a style of button created at this service */}
-      <Button>Show Detail</Button>
+      <Button
+        onClick={() => {
+          location.href = "#details"
+        }}
+        style={{
+          padding: "0",
+          borderRadius: "20px",
+        }}
+      >
+        <img src={`${location.origin}/buttons/simple.svg?name=Show%20Detail`} />
+      </Button>
     </Box>
-    <Box className={classes.detail}>
+    <Box className={classes.detail} id="details">
       <Typography variant="h3">Details</Typography>
 
       <Box className={classes.explanation}>
         <Typography variant="h4">1. Create your GitHub Actions to build with human based trigger.</Typography>
-        <Typography variant="body1">aaaa a aaa aaa a aaa</Typography>
+        <Typography variant="body1" className={classes.body}>
+          GitHub Actions is useful feature on GitHub.
+          Actions is able to run with various GitHub events such as pushes or pull requests. 
+          However there is no event with human based trigger such as click any button. 
+          This service makes it possible to run with clicking a button. 
+          For example, setup deploy feature in this button. 
+          <a href="https://github.com/kouki-dan/action-button/blob/master/.github/workflows/deploy.yml">This</a> is an example to use this button.
+        </Typography>
       </Box>
 
       <Box className={classes.explanation}>
         <Typography variant="h4">2. Create a Button in this service.</Typography>
-        <Typography variant="body1">aaaa a aaa aaa a aaa</Typography>
+        <Typography variant="body1" className={classes.body}>To create a button is too easy. Find your repository and input a title of button and event_type name.</Typography>
+        <div style={{
+          textAlign: "center"
+        }}
+        >
+          <img
+            width="80%"
+            src="https://user-images.githubusercontent.com/1401711/65517425-a9f59380-df1d-11e9-8ef2-b94b7ecd4df6.png"
+          />
+        </div>
       </Box>
 
       <Box className={classes.explanation}>
         <Typography variant="h4">3. Put the Button to your README at your GitHub repository.</Typography>
-        <Typography variant="body1">aaaa a aaa aaa a aaa</Typography>
+        <Typography variant="body1" className={classes.body}>
+          After creating a button, copy markdown text and paste to your GitHub page to run actions/
+        </Typography>
       </Box>
 
       <Box className={classes.explanation}>
         <Typography variant="h4">4. Click the Button in README file and trigger GitHub Actions anywhere.</Typography>
-        <Typography variant="body1">aaaa a aaa aaa a aaa</Typography>
+        <Typography variant="body1" className={classes.body}>Click the button and run Actions.</Typography>
+        <div style={{
+          textAlign: "center"
+        }}
+        >
+          <img
+            width="80%"
+            src="https://user-images.githubusercontent.com/1401711/65518509-5ab06280-df1f-11e9-9f29-c1d3c6055402.png"
+          />
+        </div>
+
       </Box>
 
       <Box style={{
         textAlign: "center"
       }}>
-        <Button>Try it now!</Button>
+        <Link to="repos">
+          <Button
+            style={{
+              padding: "0",
+              borderRadius: "20px",
+            }}
+          >
+            <img src={`${location.origin}/buttons/simple.svg?name=Try%20it%20now!`} />
+          </Button>
+        </Link>
       </Box>
     </Box>
   </>;
